8565d2d932d1a1fedb72dc7751690aeeb620cf48
LSH <dltpgns97@gmail.com>
2022-04-04 15:45:18 +0900

    react practice

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,15 +1,17 @@
-import movie from "../Typing";
+import movie from "../Typing/Typing";
+import {Link } from "react-router-dom";
 
 function Movie(props:movie){
     return(
         <div>
             <img src={props.medium_cover_image} / >
-            <h2>{props.title}</h2>
+            <h2>
+                <Link to={`/movie/${props.id}`}>{props.title}</Link>
+            </h2>
             <p>{props.summary}</p>
             <ul>
                 {props.genres.map(g =>
-                <li key={g}>{g}</li>
-                )}
+                <li key={g}>{g}</li>)}
             </ul>
         </div>
     );
---
+++ src/routes/Home.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Movie from '../components/Movie';
+import movie from "../Typing/Typing";
+
+function Home(){
+    const [loading, setLoaning] = useState(true);
+    const [movies, setMovies] = useState<movie[]>([]);
+
+    const getMovies= async() => {
+        const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year');
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoaning(false);
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+        <div>
+            {loading ? 
+                (<h1>Loading...</h1>) : 
+                (<div>
+                {movies.map((movie) => (
+                <Movie 
+                key={movie.id}
+                {...movie}/>))}
+                </div>)
+            }
+        </div>
+    )
+}
+
+export default Home;
